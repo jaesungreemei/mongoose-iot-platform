4f1f0232829e14187268717ae27eafabe623aae9
seok0301 <seok990301@gmail.com>
2024-02-14 18:48:30 +0900

    Update: 쉘 스크립트

--- wsl-scripts/run-init.sh
+++ wsl-scripts/run-init.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-sed -i 's/\r$//' wsl-scripts/1-init.sh
-sed -i 's/\r$//' wsl-scripts/2-init-kafka.sh
-sed -i 's/\r$//' wsl-scripts/3-init-cassandra.sh
-sed -i 's/\r$//' wsl-scripts/4-init-kafka-connect.sh
-sed -i 's/\r$//' wsl-scripts/5-init-materials.sh
-sudo bash wsl-scripts/1-init.sh
-sudo bash wsl-scripts/2-init-kafka.sh
-sudo bash wsl-scripts/3-init-cassandra.sh
-sudo bash wsl-scripts/4-init-kafka-connect.sh
-sudo bash wsl-scripts/5-init-materials.sh
+
+# 스크립트 실행 시 오류 발생 시 중단
+set -e
+
+# 스크립트 파일들의 경로 배열
+scripts=(
+    "wsl-scripts/1-init.sh"
+    "wsl-scripts/2-init-kafka.sh"
+    "wsl-scripts/3-init-cassandra.sh"
+    "wsl-scripts/4-init-kafka-connect.sh"
+    "wsl-scripts/5-init-materials.sh"
+)
+
+# Unix 스타일의 줄바꿈으로 변환 및 스크립트 실행
+for script in "${scripts[@]}"; do
+    sed -i 's/\r$//' "$script" # Windows 줄바꿈(CR) 제거
+    bash "$script"             # 스크립트 실행
+done
+
+echo "All scripts executed successfully."
